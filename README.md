5c29fad601298983e83de1717384a688c58b0ba2
Umang Galaiya <umang.galaiya@razorpay.com>
2018-02-25 21:32:58 +0530

    Adds library, doc, license.

---
+++ README.md
@@ -0,0 +1,120 @@
+# Swipe-Listener
+
+Zero-dependency, minimal swipe-gesture listener for the web.
+
+---
+
+## [Demo](https://umanghome.github.io/swipe-listener) 
+
+Open from a touch-device, or simulate a touch-device using DevTools.
+
+# What
+
+Swipe-listener is a very minimal library that allows listening for swipe gesture on literally any DOM element. Once invoked with a DOM element, simply listen for `onswipe` event and determine the direction with the `directions` object.
+
+# Example Code
+
+```js
+var container = document.querySelector('#container');
+SwipeListener(container);
+container.addEventListener('onswipe', function (e) {
+  var directions = e.detail.directions;
+  var x = e.detail.x;
+  var y = e.detail.y;
+
+  if (directions.left) {
+    console.log('Swiped left.');
+  }
+
+  if (directions.right) {
+    console.log('Swiped right.');
+  }
+
+  if (directions.top) {
+    console.log('Swiped top.');
+  }
+
+  if (directions.bottom) {
+    console.log('Swiped bottom.');
+  }
+
+  if (directions.top && directions.right) {
+    console.log('Swiped top-right.');
+  }
+
+  if (directions.bottom && directions.left) {
+    console.log('Swiped bottom-left.');
+  }
+
+  console.log('Started horizontally at', x[0], 'and ended at', x[1]);
+
+  console.log('Started vertically at', y[0], 'and ended at', y[1]);
+});
+```
+
+# Installation
+
+## Browser
+
+```html
+<script src="path/to/swipe-listener.min.js" type="text/javascript"></script>
+<script>
+  var container = document.querySelector('#container');
+  SwipeListener(container);
+  container.addEventListener('onswipe', function (e) {
+    console.log(e.detail);
+  });
+</script>
+```
+
+Swipe-listener is also available from unpkg: [`https://unpkg.com/swipe-listener@1.0.0/swipe-listener.min.js`](https://unpkg.com/swipe-listener@1.0.0/swipe-listener.min.js)
+
+## Installing using NPM
+
+Install from NPM using `npm i --save swipe-listener`, then
+
+```js
+import SwipeListener from 'swipe-listener';
+```
+
+OR
+
+```js
+const SwipeListener = require('swipe-listener');
+```
+
+# API
+
+### `SwipeListener(element, options)`
+
+- `element` DOM Element on which you want to enable swipe gesture tracking. This is the element on which you will be attacking the `onswipe` event listener.
+- `options` [Optional] Configuration options (see below)
+
+Listen for `onswipe` event on the `element` passed. Access details using `event.detail`. For example, `directions` can be accessed using `event.detail.directions`.
+
+Data passed to `event.detail`:
+
+- `directions` (Object)
+  - `top` (Boolean) Signifies a top-swipe.
+  - `right` (Boolean) Signifies a right-swipe.
+  - `bottom` (Boolean) Signifies a bottom-swipe.
+  - `left` (Boolean) Signifies a left-swipe.
+- `x` (Array) Array containing two elements: starting and ending x-coords.
+- `y` (Array) Array containing two elements: starting and ending y-coords.
+
+Note that multiple directions can be `true` at one. In case of a top-left swipe, `directions.top` and `directions.left` will both be `true`.
+
+### Options
+
+| Key | Description | Default value |
+| --- | --- | --- |
+| `minHorizontal` | Minimum number of horizontal pixels travelled for the gesture to be considered as a left or right swipe. | `10` |
+| `minVertical` | Minimum number of vertical pixels travelled for the gesture to be considered as a top or bottom swipe. | `10` |
+| `deltaHorizontal` | Maximum difference between the rightmost pixel (right-swipe) or the leftmost pixel (left-swipe) travelled to and the pixel at which the gesture is released. | `3` |
+| `deltaVertical` | Maximum difference between the bottommost pixel (bottom-swipe) or the topmost pixel (top-swipe) travelled to and the pixel at which the gesture is released. | `5` |
+
+---
+
+# License
+
+[MIT License](https://opensource.org/licenses/MIT) © [Umang Galaiya](https://umanggalaiya.in/)
---
+++ swipe-listener.js
@@ -0,0 +1,193 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+/**
+ * Starts monitoring swipes on the given element and
+ * emits `onswipe` event when a swipe gesture is performed.
+ * @param {DOMElement} element Element on which to listen for swipe gestures.
+ * @param {Object} options Optional: Options.
+ */
+var SwipeListener = function SwipeListener(element, options) {
+  if (!element) return;
+
+  // CustomEvent polyfill
+  if (typeof window !== 'undefined') {
+    (function () {
+      if (typeof window.CustomEvent === 'function') return false;
+      function CustomEvent(event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+      }
+      CustomEvent.prototype = window.Event.prototype;
+      window.CustomEvent = CustomEvent;
+    })();
+  }
+
+  var defaultOpts = {
+    minHorizontal: 10, // Minimum number of pixels traveled to count as a horizontal swipe.
+    minVertical: 10, // Minimum number of pixels traveled to count as a vertical swipe.
+    deltaHorizontal: 3, // Delta for horizontal swipe
+    deltaVerical: 5 // Delta for vertical swipe
+  };
+
+  // Set options
+  if (!options) {
+    options = {};
+  }
+  options = _extends({}, defaultOpts, options);
+
+  /**
+   * Returns the minimum value from the array.
+   * @param {Array} arr 
+   * @return {Any}
+   */
+  function findMin(arr) {
+    if (!arr || !arr.length) return;
+    var m = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < m) {
+        m = arr[i];
+      }
+    }
+    return m;
+  }
+
+  /**
+   * Returns the maximum value from the array.
+   * @param {Array} arr 
+   * @return {Any}
+   */
+  function findMax(arr) {
+    if (!arr || !arr.length) return;
+    var m = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > m) {
+        m = arr[i];
+      }
+    }
+    return m;
+  }
+
+  // Store the touches
+  var touches = [];
+
+  // When a swipe is performed, store the coords.
+  element.addEventListener('touchmove', function (e) {
+    var touch = e.changedTouches[0];
+    touches.push({
+      x: touch.clientX,
+      y: touch.clientY
+    });
+  });
+
+  // When the swipe is completed, calculate the direction.
+  element.addEventListener('touchend', function (e) {
+    if (!touches.length) return;
+
+    var x = [];
+    var y = [];
+
+    var directions = {
+      top: false,
+      right: false,
+      bottom: false,
+      left: false
+    };
+
+    for (var i = 0; i < touches.length; i++) {
+      x.push(touches[i].x);
+      y.push(touches[i].y);
+    }
+
+    // Determine left or right
+    var diff = x[0] - x[x.length - 1];
+    var swipe = 'none';
+    if (diff > 0) {
+      swipe = 'left';
+    } else {
+      swipe = 'right';
+    }
+
+    var min = findMin(x),
+        max = findMax(x),
+        _diff = void 0;
+
+    // If minimum horizontal distance was travelled
+    if (Math.abs(diff) >= options.minHorizontal) {
+      switch (swipe) {
+        case 'left':
+          _diff = Math.abs(min - x[x.length - 1]);
+          if (_diff <= options.deltaHorizontal) {
+            directions.left = true;
+          }
+          break;
+        case 'right':
+          _diff = Math.abs(max - x[x.length - 1]);
+          if (_diff <= options.deltaHorizontal) {
+            directions.right = true;
+          }
+          break;
+      }
+    }
+
+    // Determine top or bottom
+    diff = y[0] - y[y.length - 1];
+    swipe = 'none';
+    if (diff > 0) {
+      swipe = 'top';
+    } else {
+      swipe = 'bottom';
+    }
+
+    min = findMin(y);
+    max = findMax(y);
+
+    // If minimum vertical distance was travelled
+    if (Math.abs(diff) >= options.minVertical) {
+      switch (swipe) {
+        case 'top':
+          _diff = Math.abs(min - y[y.length - 1]);
+          if (_diff <= options.deltaVerical) {
+            directions.top = true;
+          }
+          break;
+        case 'bottom':
+          _diff = Math.abs(max - y[y.length - 1]);
+          if (_diff <= options.deltaVerical) {
+            directions.bottom = true;
+          }
+          break;
+      }
+    }
+
+    // Clear touches array.
+    touches = [];
+
+    // If there is a swipe direction, emit an event.
+    if (directions.top || directions.right || directions.bottom || directions.left) {
+      var event = new CustomEvent('onswipe', {
+        detail: {
+          directions: directions,
+          x: [x[0], x[x.length - 1]], // Start and end x-coords
+          y: [y[0], y[y.length - 1]] // Start and end y-coords
+        }
+      });
+      element.dispatchEvent(event);
+    }
+  });
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = SwipeListener;
+} else {
+  if (typeof define === 'function' && define.amd) {
+    define([], function () {
+      return SwipeListener;
+    });
+  } else {
+    window.SwipeListener = SwipeListener;
+  }
+}
