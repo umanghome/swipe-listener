1801bae7a16e7dd852f43e79ba0b4b503d29d4d2
Umang Galaiya <umanghome@gmail.com>
2024-08-26 00:07:28 +0530

    Separate minified and unminified files

--- rollup.config.js
+++ rollup.config.js
@@ -6,17 +6,34 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/index.ts',
   output: [
+    // Unminified UMD output
     {
       file: 'dist/swipe-listener.js',
       format: 'umd',
       name: 'SwipeListener',
       sourcemap: true,
     },
+    // Minified UMD output
+    {
+      file: 'dist/swipe-listener.min.js',
+      format: 'umd',
+      name: 'SwipeListener',
+      sourcemap: true,
+      plugins: [terser()], // Minifies this output
+    },
+    // Unminified ESM output
     {
       file: 'dist/swipe-listener.mjs',
       format: 'esm',
       sourcemap: true,
     },
+    // Minified ESM output
+    {
+      file: 'dist/swipe-listener.min.mjs',
+      format: 'esm',
+      sourcemap: true,
+      plugins: [terser()], // Minifies this output
+    },
   ],
-  plugins: [resolve(), commonjs(), typescript(), terser()],
+  plugins: [resolve(), commonjs(), typescript()],
 };
